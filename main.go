f5c54d97a3499ddaae6350279aef1421df1099d0
Ronnie <ckzks2013@gmail.com>
2022-07-17 18:48:06 -0700

    play with const

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	firstName := "namgi"
-	fmt.Println(firstName)
-
-	ptr := &firstName
-	fmt.Println(ptr, *ptr)
+	const pi = 3.14
+	fmt.Println(float32(pi)+3)
 }
